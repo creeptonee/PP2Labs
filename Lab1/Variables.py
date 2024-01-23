a7d319f4f93237fd4bd7586e3b60462e88d1d4b5
admin <n_basheyev@kbtu.kz>
2024-01-23 13:23:26 +0500

    .

---
+++ Lab1/Variables.py
@@ -0,0 +1,31 @@
+# EX1
+carname = "Volvo"
+
+#EX2
+x = 50
+
+#EX3
+x=5
+y=10
+print(x+y)
+
+
+#EX4
+x=5
+y=10
+z = x+y
+print(z)
+
+#Ex5
+x, y, z = "Orange", "Banana", "Cherry"
+
+#Ex6
+x = y = z = "Orange"
+
+#Ex7
+def myFunc():
+    global x
+    x = "fantastic"
+
+
+
